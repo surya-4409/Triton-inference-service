27ec3c1b268ecb10473c35cbd60555c6aaf64df9
surya-4409 <suryanarayana9125@gmail.com>
2026-05-15 17:08:49 +0530

    Force overwrite of fixed files for grader

--- scripts/test_versioning.sh
+++ scripts/test_versioning.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-"C:\Users\surya\AppData\Local\Programs\Python\Python311\python.exe" scripts/test_versioning.py 2>/dev/null || python scripts/test_versioning.py 2>/dev/null || python3 scripts/test_versioning.py
+python3 scripts/test_versioning.py
